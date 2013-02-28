6db860bfab1ba526a771292f8a3f1dd2c9ea78d5
chris.zen <fzhenmei@gmail.com>
2013-02-28 17:31:24 +0800

    现在可以调用C++函数了
    会者不难，但是第一次这么干还是花了不少时间。

--- calldll/Unit1.pas
+++ calldll/Unit1.pas
@@ -32,6 +32,8 @@ type
     { Public declarations }
   end;
 
+  //TCharArray = array of char;
+
   PPosProxyConfig = ^TPosProxyConfig;
   TPosProxyConfig = record
     size: Integer;
@@ -49,27 +51,128 @@ type
     ihhsecs: Integer;
     idtsecs: Integer;
   end;
-  
+
+  TradeReq = ^TTradeReq;
+  TTradeReq = record
+    cType : Char;
+    payType : Char;
+    carrTradeSum : array[0..12] of char;
+    carrTradeSN : array [0..12] of char;
+    carrTermSN : array[0..6] of char;
+    sType : array [0..2] of char;
+    czTran : array [0..19] of char;
+    czType : array [0..2] of char;
+    payName : array [0..10] of char;
+  end;
+
+  TradeRsp = ^TTradeRsp;
+  TTradeRsp = record
+    carrTradeSum : array [0..12] of char;
+    carrRsp : array [0..2] of char;
+    carrTermNum : array [0..8] of char;
+    carrMerchantNo : array [0..15] of char;
+    carrTermSN : array [0..6] of char;
+    settlementDate : array [0..4] of char;
+    carrCardNo : array [0..20] of char;
+    carrCardBankNo : array[0..8] of char;
+    carrCardBankName : array[0..8] of char;
+    carrActualSum : array[0..12] of char;
+    carrSysRefNo : array[0..12] of char;
+    queryRealBal : array[0..12] of char;
+    maxDiscount : array[0..2] of char;
+    exchangeJFCount : array[0..12] of char;
+    exchangeFLAmount : array[0..12] of char;
+    exchangeZJAmount : array[0..12] of char;
+    fundConsumeAmount : array[0..12] of Char;
+    giftConsumeAmount : array[0..12] of char;
+    lotteryName1 : array[0..10] of char;
+    lotteryName2 : array[0..10] of char;
+    lotteryName3 : array[0..10] of char;
+    commonECGiftAmount : array[0..12] of char;
+    exclusECGiftAmount : array[0..12] of char;
+    commonPaperGiftNum : array[0..12] of Char;
+    exclusPaperGiftNum : array[0..12] of char;
+    businessECGiftAmount : array[0..12] of char;
+    businessPaperGiftNum : array[0..12] of char;
+    commonJFCount : array[0..12] of char;
+    exclusJFCount : array[0..12] of char;
+    businessJFCount : array[0..12] of char;
+    otherJFCount : array[0..12] of char;
+    consumeDiscType : Char;
+    realDiscountAmount : array[0..2] of char;
+  end;
+
+
   function PosProxyInit(configfile : PChar; pProxyConfig : PPosProxyConfig) : THandle; stdcall; external 'Proxydll.dll';
+  function PosProxyDoTrade(handler : THandle; req : TradeReq; rep : TradeRsp ) : THandle; stdcall; external 'Proxydll.dll';
+  function PosProxyUninit() : THandle; stdcall; external 'Proxydll.dll'
 
 var
   Form1: TForm1;
+  InitHnadler: THandle;
 
 implementation
 
 {$R *.dfm}
 
+{function StringToCharArray(str : string) : TCharArray;
+  var
+    charArray : TCharArray;
+    len : Integer;
+    i : Integer;
+begin
+  len := Length(str);
+  SetLength(charArray, Length(str));
+  for i := 0 to len do
+  begin
+    charArray[i] := str[i];
+  end;
+  //copymemory(@charArray, pchar(str), Length(str));
+  Result := charArray;
+end;}
+
 procedure TForm1.btnCallDllClick(Sender: TObject);
 var
    config:TPosProxyConfig;
 begin
-  PosProxyInit('config.ini', nil);
-  Application.MessageBox('看到这个对话框就证明调用成功。', '测试');
+  InitHnadler := PosProxyInit('config.ini', nil);
+  btnCallDll.Enabled := False;
 end;
 
 procedure TForm1.btnTradeClick(Sender: TObject);
+  var
+    t_request : TTradeReq;
+    t_response : TTradeRsp;
+    p_request : TradeReq;
+    p_response : TradeRsp;
+    money : array[0..12] of char;
 begin
-  //start trade
+  t_request.cType := '0';
+  t_request.payType := '1';
+
+  t_request.carrTradeSum[0] := '0';
+  t_request.carrTradeSum[1] := '0';
+  t_request.carrTradeSum[2] := '0';
+  t_request.carrTradeSum[3] := '0';
+  t_request.carrTradeSum[4] := '0';
+  t_request.carrTradeSum[5] := '0';
+  t_request.carrTradeSum[6] := '0';
+  t_request.carrTradeSum[7] := '0';
+  t_request.carrTradeSum[8] := '0';
+  t_request.carrTradeSum[9] := '0';
+  t_request.carrTradeSum[10] := '0';
+  t_request.carrTradeSum[11] := '0';
+  t_request.carrTradeSum[12] := '1';
+
+  t_request.carrTermSN[0] := '0';
+  t_request.carrTermSN[1] := '0';
+  t_request.carrTermSN[2] := '0';
+  t_request.carrTermSN[3] := '0';
+  t_request.carrTermSN[4] := '0';
+  t_request.carrTermSN[5] := '0';
+  t_request.carrTermSN[6] := '1';
+  
+  PosProxyDoTrade(InitHnadler, @t_request, @t_response);
 end;
 
 procedure TForm1.btnQuitClick(Sender: TObject);
